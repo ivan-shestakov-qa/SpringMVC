2a6a48f2123b47f9c9e6d87016a764f8335efd5a
ivan-shestakov-qa <git@github.com:ivan-shestakov-qa/Java_Spring.git>
2024-05-02 12:23:33 +0300

    springMvc leson3/1

--- src/main/java/by/shestakov/controllers/FirstController.java
+++ src/main/java/by/shestakov/controllers/FirstController.java
@@ -1,12 +1,16 @@
 package by.shestakov.controllers;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class FirstController {
     @GetMapping("/hello")
-    public String helloPage(){
+    public String helloPage(HttpServletRequest request){
+        String name = request.getParameter("name");
+        String surname = request.getParameter("surname");
+        System.out.println("Hello, " + name + " " + surname);
         return "first/hello";
     }
     @GetMapping("/goodbye")

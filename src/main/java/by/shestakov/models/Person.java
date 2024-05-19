e2dfc14d5c958f4cdc41007d0639e746030c5c3b
ivan-shestakov-qa <git@github.com:ivan-shestakov-qa/Java_Spring.git>
2024-05-19 12:55:58 +0300

    springMvc leson4

---
+++ src/main/java/by/shestakov/models/Person.java
@@ -0,0 +1,15 @@
+package by.shestakov.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Person {
+    private int id;
+    private String name;
+}
